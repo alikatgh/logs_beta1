ca886cb6a8dbaaf9c93d9167db724b4430ef18a3
alikatgh <i@alikn.ru>
2024-12-02 10:38:22 +0800

    min stable

--- app/main_routes.py
+++ app/main_routes.py
@@ -1,3 +1,4 @@
+"""Main routes for the application."""
 from flask import Blueprint, render_template
 from flask_login import login_required
 
@@ -8,4 +9,5 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 @login_required
 def index():
+    """Render the home page."""
     return render_template('index.html') 
